8df7dd5bfb37e6e0eb7e502bd673895f024840ca
Benjamin <62201510+Jaminanim@users.noreply.github.com>
2025-01-06 07:56:56 +1100

    Update __init__.py
    Add adanvced node.

--- __init__.py
+++ __init__.py
@@ -1,12 +1,15 @@
 from .random_integer_node_efficient import RandomIntegerNodeEfficient
 from .random_integer_node_list import RandomIntegerNodeList
+from .random_integer_node_efficient_advanced import RandomIntegerNodeEfficientAdvanced
 
 NODE_CLASS_MAPPINGS = {
-    "RandomIntegerNodeEfficient": RandomIntegerNodeEfficient,
-    "RandomIntegerNodeList": RandomIntegerNodeList,
-}
+       "RandomIntegerNodeEfficient": RandomIntegerNodeEfficient,
+       "RandomIntegerNodeList": RandomIntegerNodeList,
+       "RandomIntegerNodeEfficientAdvanced": RandomIntegerNodeEfficientAdvanced,
+   }
 
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "RandomIntegerNodeEfficient": "Efficient Random Integer Generator",
-    "RandomIntegerNodeList": "Random Integer Generator Using List",
-}
+       "RandomIntegerNodeEfficient": "Efficient Random Integer Generator",
+       "RandomIntegerNodeList": "Random Integer Generator Using List",
+       "RandomIntegerNodeEfficientAdvanced": "Advanced Random Integer Generator",
+   }
